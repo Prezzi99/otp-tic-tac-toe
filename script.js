76dbe79d0da72bc3122a5efb53b1ff45aff8dca9
Ishmael Reid <ishmaelreid9@gmail.com>
2024-03-28 22:55:06 -0400

    Added a factory function to control the game flow
    The Game() function creates an array with two
    players, these are objects that have a
    name and mark. The factory encapsulates
    the following methods:
    
    SwitchPlayer - Gives control to the next player
    
    MakePlay - Adds the active player's mark to the
    gameboard.
    
    GetActivePlayer - Returns the player who currently
    has control.
    
    The active player is set to player one at the
    offset of the game. The player is switched after
    every valid play.

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-function GameBoard() {
+const board = (function GameBoard() {
     const board = [];
     const rows = 3;
     const columns = 3;
 
-    // Add rows (arrays) and columns (cells/objects) to the game board. 
-    // A column is denoted by the series of cells in each row
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push(Cell());
-        }
-    }
+    NewBoard()
 
     const UpdateBoard = (row, column, mark) => {
         // If the cell doesn't have a mark. Add the players mark.
@@ -31,8 +24,19 @@ function GameBoard() {
         console.log(gameBoard);
     };
 
-    return { UpdateBoard, PrintBoard }
-}
+    function NewBoard() {
+        // Add rows (arrays) and columns (cells/objects) to the game board. 
+        // A column is denoted by the series of cells in each row
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < columns; j++) {
+                board[i].push(Cell());
+            }
+        }
+    }
+
+    return { UpdateBoard, PrintBoard, NewBoard };
+})();
 
 function Cell() {
     const cell = {};
@@ -43,12 +47,43 @@ function Cell() {
 
     const GetMark = () => cell.mark;
 
-    return { AddMark, GetMark }
+    return { AddMark, GetMark };
 }
 
-function GameController() {
+// For controlling the game flow.
+function Game(playerOne = 'Player One', playerTwo = 'Player Two') {
+    // Create players
+    const players = [
+        {playerOne, mark: 'X'},
+        {playerTwo, mark: 'O'}
+    ]
 
+    // Play round
+    let player = players[0];
+    function MakePlay(row, column, player) {
+        validPlay = gameboard.UpdateBoard(row, column, player.mark);
+        (validPlay) ? SwitchPlayer(player) : console.log('Invalid Play');
+    }
+
+
+    // Switch player
+    function SwitchPlayer(activePlayer) {
+        if (activePlayer.mark === 'X') {
+            player = players[1];
+        }
+        else if (activePlayer.mark === 'O') {
+            player = players[0];
+        }
+    }
+
+    const getActivePlayer = () => player;
+
+    return { MakePlay, getActivePlayer };
 }
 
-const board = GameBoard();
-board.PrintBoard();
+// const board = GameBoard();
+// board.PrintBoard();
+// board.UpdateBoard(0, 0, 'X');
+// board.PrintBoard();
+// board.NewBoard();
+// board.PrintBoard();
