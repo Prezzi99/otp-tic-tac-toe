ec31961da9a5f51e2f0cdfa29e004b458e604a37
Ishmael Reid <ishmaelreid9@gmail.com>
2024-03-29 12:03:34 -0400

    Changed the location of two functions
    I moved Cell() from outside the of the Game
    factory and encapsulated in the GameBoard module.
    It is a more appropriate location because Cell()
    is only invoked in GameBoard.
    
    I moved PrintBoard form the GameBoard module
    the Game factory function because I want to allow
    users to print board at any time throughout the
    game.
    
    FindWinner is now able to determine when there's
    a winning row or column and return the winner.

--- script.js
+++ script.js
@@ -1,57 +1,51 @@
-const gameBoard = (function() {
-    const board = [];
-    const rows = 3;
-    const columns = 3;
-
-    NewBoard()
-
-    const UpdateBoard = (row, column, mark) => {
-        // If the cell doesn't have a mark. Add the players mark.
-        if (!board[row][column].GetMark()) {
-            board[row][column].AddMark(mark);
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    const PrintBoard = () => {
-        let gameBoard = [];
-        board.forEach((row) => {
-            gameBoard.push(row.map((cell) => cell.GetMark()));
-        });
-        console.log(gameBoard);
-    };
-
-    function NewBoard() {
-        // Add rows (arrays) and columns (cells/objects) to the game board. 
-        // A column is denoted by the series of cells in each row
-        for (let i = 0; i < rows; i++) {
-            board[i] = [];
-            for (let j = 0; j < columns; j++) {
-                board[i].push(Cell());
-            }
-        }
-    }
-
-    return { UpdateBoard, PrintBoard, NewBoard };
-})();
-
-function Cell() {
-    const cell = {mark: ''};
-
-    const AddMark = (playerMark) => {
-        cell.mark = playerMark;
-    }
-
-    const GetMark = () => cell.mark;
-
-    return { AddMark, GetMark };
-}
-
 // For controlling the game flow.
 function Game(playerOne = 'Player One', playerTwo = 'Player Two') {
+    const gameBoard = (function() {
+        const board = [];
+        const rows = 3;
+        const columns = 3;
+    
+        NewBoard()
+    
+        const UpdateBoard = (row, column, mark) => {
+            // If the cell doesn't have a mark. Add the players mark.
+            if (!board[row][column].GetMark()) {
+                board[row][column].AddMark(mark);
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    
+        function NewBoard() {
+            // Add rows (arrays) and columns (cells/objects) to the game board. 
+            // A column is denoted by the series of cells in each row
+            for (let i = 0; i < rows; i++) {
+                board[i] = [];
+                for (let j = 0; j < columns; j++) {
+                    board[i].push(Cell());
+                }
+            }
+        }
+
+        function Cell() {
+            const cell = {mark: ''};
+        
+            const AddMark = (playerMark) => {
+                cell.mark = playerMark;
+            }
+        
+            const GetMark = () => cell.mark;
+        
+            return { AddMark, GetMark };
+        }
+
+        function GetBoard() { return board }
+    
+        return { UpdateBoard, NewBoard, GetBoard };
+    })();
+
     // Count past plays
     let playCount;
 
@@ -66,7 +60,9 @@ function Game(playerOne = 'Player One', playerTwo = 'Player Two') {
     function MakePlay(row, column, player) {
         validPlay = gameBoard.UpdateBoard(row, column, player.mark);
         if (validPlay) playCount++;
-        FindWinner({row, column});
+        winner = FindWinner({row, column});
+        
+        // Switch player if the play is valid and is not a winning move
         (validPlay) ? SwitchPlayer(player) : console.log('Invalid Play');
     }
 
@@ -80,36 +76,45 @@ function Game(playerOne = 'Player One', playerTwo = 'Player Two') {
         }
     }
 
-    const getActivePlayer = () => player;
+    function PrintBoard() {
+        let board = [];
+        gameBoard.GetBoard().forEach((row) => {
+            board.push(row.map((cell) => cell.GetMark()));
+        });
+        console.log(board);
+    };
+
+    function FindWinner(indecies) {
+        // Abort if there are less than 3 marks on the board.
+        if (playCount < 3) return;
     
-    return { MakePlay, getActivePlayer };
-}
-
-function FindWinner(indecies) {
-    // Abort if there are less than 3 marks on the board.
-    if (playCount < 3) return;
-
-    // Check the index position of the last play and then evaluate contiguous cells.
-    let gameWon;
-
-    // Check the row of the last play
-    gameWon = Boolean(board[indecies.row].map((cell) => cell.mark).reduce(accumulator));
-
-    // Check the column of the last play
-    const column = [];
-    for (let i = 0; i < 3; i++) column.push(board[i][indecies.column]);
-    gameWon = Boolean(column.map((cell) => cell.mark)).reduce(accumulator);
-
-    // Check for diagonal pattern
+        // Check the index position of the last play and then evaluate contiguous cells.
+        let gameWon;
     
-    function accumulator(accumulator, mark) {
-        return (accumulator === mark) ? accumulator = mark : accumulator = false;
+        // Check the row of the last play
+        const newRow = gameBoard.GetBoard()[indecies.row]
+        gameWon = Boolean(gameBoard.GetBoard()[indecies.row].map((cell) => cell.GetMark()).reduce(accumulator));
+        if (gameWon) return player;
+    
+        // Check the column of the last play
+        const column = [];
+        for (let i = 0; i < 3; i++) column.push(gameBoard.GetBoard()[i][indecies.column]);
+        gameWon = Boolean(column.map((cell) => cell.GetMark()).reduce(accumulator));
+        console.log(column.map((cell) => cell.GetMark()));
+        if (gameWon) return player;
+    
+    
+        // Check for diagonal pattern
+        
+        function accumulator(accumulator, mark) {
+            return (accumulator === mark) ? accumulator = mark : accumulator = false;
+        }
     }
+
+    const getActivePlayer = () => player;
+    // const showUs = (PrintBoard)
+
+    return { MakePlay, getActivePlayer, PrintBoard};
 }
 
-// const board = GameBoard();
-board.PrintBoard();
-// board.UpdateBoard(0, 0, 'X');
-// board.PrintBoard();
-// board.NewBoard();
-// board.PrintBoard();
+const ticTacToe = Game();
